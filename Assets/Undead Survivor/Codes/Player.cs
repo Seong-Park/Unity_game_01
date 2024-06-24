decb937f4e704aa513df6ec5e70da182852932ed
Seong-Park <151607864+Seong-Park@users.noreply.github.com>
2024-06-24 15:32:54 +0900

    시작

---
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Inputsystem을 설치해서 사용하고 있으니 아래의 코드는 무용.
+    //void Update()
+    //{
+    //    inputVec.x = Input.GetAxisRaw("Horizontal"); //그냥 GetAxis는 속도 보정이 있음. 약간 미끄러지듯?
+    //    inputVec.y = Input.GetAxisRaw("Vertical");
+    //}
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+
+        // 1. 힘을 준다
+        //rigid.AddForce(inputVec);
+
+        // 2. 속도 제어
+        //rigid.velocity = inputVec;
+
+        // 3. 위치 이동
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    // using UnityEngine.InputSystem; 덕분에 코드 한 줄로 Moving 가능.
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
+    }
+}
